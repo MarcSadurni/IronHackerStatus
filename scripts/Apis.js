1a6858d72c1a02a3ff991fa3a8c6efea91b13fc8
MarcSadurni <marcsadurni@LAPTOP-R7RNDFA3.localdomain>
2020-10-14 17:09:30 +0200

    añadida la funcion que devuelve el gif especifico

--- scripts/Apis.js
+++ scripts/Apis.js
@@ -1,45 +1,68 @@
 
 // API PARA QUOTES
 
-// const getQuotes = () => {
-//     let randomQuotes = document.querySelector(".random-quote");
-//     fetch ('https://type.fit/api/quotes')  // obtengo la lista de las quotes
-//     .then((response) => response.json()) //parse JSON
-//     .then((texts) =>texts[Math.floor(Math.random()*texts.length)]) // obtener un texto aleatorio
-//     .then((texto) => {let finalQuote = document.createElement("p")
-//     finalQuote.innerHTML = `${texto.text.toUpperCase()}`
-//     randomQuotes.appendChild(finalQuote)
-// })
-// return texto
-//     };
-//     getQuotes()
+const getQuotes = () => {
+    let randomQuotes = document.querySelector(".random-quote");
+    fetch ('https://type.fit/api/quotes')  // obtengo la lista de las quotes
+    .then((response) => response.json()) //parse JSON
+    .then((texts) =>texts[Math.floor(Math.random()*texts.length)]) // obtener un texto aleatorio
+    .then((texto) => {let finalQuote = document.createElement("p")
+    finalQuote.innerHTML = `${texto.text.toUpperCase()}`
+    randomQuotes.appendChild(finalQuote)
+})
+return texto
+    };
+    getQuotes()
 
-    
+
 // API PARA GIFS
+let radioButtons = document.getElementsByTagName("input");
 
-function getData(){
-    let radioButtons = document.getElementsByTagName("input");
+function getData(arr) {
+
     let score = 0;
-    for ( var i=0; i< radioButtons.length; i++){
-    if(radioButtons[i].checked){
-    score += parseInt(radioButtons[i].value);
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].checked) {
+            score += parseInt(arr[i].value);
+        }
     }
-}
-console.log(score);
+    return score
 }
 
-    let getGifs = document.querySelector(".random-gif")
-    function randomGifs(mood) {
-        fetch(`https://api.giphy.com/v1/gifs/random?api_key=xA4rwVqIitvGbb1MJdR551Al4vFYrf8m&tag=${mood}&rating=g`)
-            .then((response) => {
-                return response.json()
-            })
-            .then((gift) => {
-     console.log(gift.data.embed_url)
-            })
-      .catch((err)=>{
-          console.log(err)
+function resultMood(score) {
+    let numMood = ""
+    if (score >= 4 && score <= 7) {
+        numMood = 'dead';
+    }
+    else if (score >= 8 && score <= 15) {
+        numMood = 'normal';
+    }
+    else {
+        numMood = 'happy';
+    }
+    return numMood
+}
+
+let getGifs = document.querySelector(".random-gif");
+function randomGifs(mood) {
+    console.log(mood);
+    return fetch(`https://api.giphy.com/v1/gifs/random?api_key=xA4rwVqIitvGbb1MJdR551Al4vFYrf8m&tag=${mood}&rating=g`)
+        .then((response) => {
+            return response.json()
         })
-        }
-        randomGifs('happy')
+        .then((gift) => {
+            console.log(gift.data.embed_url,"gift")
+            return gift.data.embed_url
+        })
+}
+
+async function finalGif() {
+    const number = await getData(radioButtons);
+    console.log(number, "number")
+    const resultm = await resultMood(number);
+    console.log(resultm, "resultm")
+    const finalGif = await randomGifs(resultm);
+    console.log(finalGif, "finalGif")
+    return finalGif
+}
 
